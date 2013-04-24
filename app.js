49b6ae4bc401cc6d0109a985162a318257833739
Nicholas Angeli <nicangeli@gmail.com>
2013-04-24 21:21:15 +0100

    added server code

--- app.js
+++ app.js
@@ -14,6 +14,31 @@ var usernames = {};
 io.sockets.on('connection', function(socket) {
 	// when the client emits sendchat, this listens and executes
 	socket.on('sendchat', function(data) {
-		// tell the client to execute updatechar with 2 paramaters
-	})
+		// tell the client to execute updatechat with 2 paramaters
+		io.sockets.emit('updatechat', socket.username, data);
+	});
+
+	// when the client emits adduser this listen and executes
+	socket.on('adduser', function(username) {
+		// we store the username in the socket session for this client
+		socket.username = username;
+		// add username to global list
+		usernames[username] = username;
+		// echo to client that they've connected
+		socket.emit('updatechat', 'SERVER', 'you have connected');
+		// echo globally that a person has connected
+		socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
+		// update the list of users in chat,client side
+		io.sockets.emit('updateusers', usernames);
+	});
+
+	// when the user disconnects
+	socket.on('disconnect', function() {
+		// remove username from global list
+		delete usernames[socket.username];
+		// update list of users in chat client side
+		io.sockets.emit('updateusers', usernames);
+		// echo globally that the user has left
+		socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has left');
+	});
 });
